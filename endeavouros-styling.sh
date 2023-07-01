14b17c61646dd0e6164b4ac8dc560542d7790746
k3ii <k3i@protonmail.com>
2023-07-01 19:19:03 +0400

    add Styling scripts

---
+++ endeavouros-styling.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+DISTRIBUTION=endeavouros
+HEADER=EndeavourOS
+
+SOURCE_STYLING=/usr/local/share/improved-directory-themes
+SHADES="$(cat $SOURCE_STYLING/$DISTRIBUTION-shades)"
+
+cp $SOURCE_STYLING/site-footer.html "/vol-mirror/$DISTRIBUTION-mirror/.footer.html"
+cp $SOURCE_STYLING/site-header.html "/vol-mirror/$DISTRIBUTION-mirror/.header.html"
+cp $SOURCE_STYLING/.style.css "/vol-mirror/$DISTRIBUTION-mirror/.style.css"
+
+footer="/vol-mirror/$DISTRIBUTION-mirror/.footer.html"
+header="/vol-mirror/$DISTRIBUTION-mirror/.header.html"
+style="/vol-mirror/$DISTRIBUTION-mirror/.style.css"
+
+sed -i "s/dist/$HEADER/" "$header"
+
+awk -v addpattern="$SHADES" '/Base Colors/{print $0 "\n" addpattern;next}1' $style  > /tmp/e-style.css
+mv /tmp/e-style.css $style
+
